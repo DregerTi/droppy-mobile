5d6e46a3d5d72fd3a7868370ed03c9a33ec9415c
Dregert <rui.soares282@gmail.com>
2024-07-17 11:52:15 +0200

    feat/providers

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 import 'package:droppy/features/presentation/bloc/auth/auth_state.dart';
+import 'package:droppy/features/presentation/bloc/group/goup_bloc.dart';
+import 'package:droppy/features/presentation/bloc/group_member/goup_member_bloc.dart';
 import 'package:droppy/features/presentation/bloc/has_dropped/has_dropped_bloc.dart';
 
 import 'features/presentation/bloc/auth/auth_event.dart';
+import 'features/presentation/bloc/content/content_bloc.dart';
 import 'features/presentation/bloc/feed/feed_bloc.dart';
 import 'features/presentation/bloc/follow/pending/pending_follow_bloc.dart';
 import 'l10n/l10n.dart';
@@ -51,6 +54,15 @@ class _MainAppState extends State<MainApp> {
           create: (context) => sl(),
         ),
         BlocProvider<HasDroppedBloc>(
+          create: (context) => sl(),
+        ),
+        BlocProvider<ContentBloc>(
+          create: (context) => sl(),
+        ),
+        BlocProvider<GroupsBloc>(
+          create: (context) => sl(),
+        ),
+        BlocProvider<GroupMembersBloc>(
           create: (context) => sl(),
         ),
       ],
